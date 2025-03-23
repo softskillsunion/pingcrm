5e86782c92472410354f995e6a6a4b5c61a3e17e
softskillsunion <softskillsunion@gmail.com>
2025-03-23 22:23:23 +0800

    多語系 php artisan lang:publish

---
+++ lang/zh-TW/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '登入資料錯誤',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => '登入錯誤次數過多，請於:seconds秒後重試',
+    'not_actived' => '您的帳戶尚未啟用',
+
+];
